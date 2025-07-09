8a055c6557099233181a919c038e154ad7dd71a1
VelcroGecko <charlie.willis12@outlook.com>
2025-07-09 21:48:29 +1000

    change to uv

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-import os
 import sys
-from dotenv import load_dotenv
+import os
 from google import genai
 from google.genai import types
- 
+from dotenv import load_dotenv
+
+from prompts import system_prompt
+from call_function import available_functions
 
 
 def main():
@@ -15,38 +17,42 @@ def main():
     if not args:
         print("AI Code Assistant")
         print('\nUsage: python main.py "your prompt here" [--verbose]')
-        print('Example: python main.py "How do I build a calculator app?"')
+        print('Example: python main.py "How do I fix the calculator?"')
         sys.exit(1)
-    
 
-    api_key = os.environ.get('GEMINI_API_KEY')
+    api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
 
     user_prompt = " ".join(args)
+
     if verbose:
         print(f"User prompt: {user_prompt}\n")
 
     messages = [
         types.Content(role="user", parts=[types.Part(text=user_prompt)]),
-        ]
-    
+    ]
 
     generate_content(client, messages, verbose)
-
 
 
 def generate_content(client, messages, verbose):
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
         contents=messages,
+        config=types.GenerateContentConfig(
+            tools=[available_functions], system_instruction=system_prompt
+        ),
     )
-
     if verbose:
-        print(f'Prompt tokens: {response.usage_metadata.prompt_token_count}')
-        print(f'Response tokens: {response.usage_metadata.candidates_token_count}')
-    
-    print("Response:")
-    print(response.text)
+        print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+        print("Response tokens:", response.usage_metadata.candidates_token_count)
+
+    if not response.function_calls:
+        return response.text
+
+    for function_call_part in response.function_calls:
+        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+
 
 if __name__ == "__main__":
     main()
